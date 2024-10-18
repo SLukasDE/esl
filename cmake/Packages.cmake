be8b640f434954c4252937885e61c7b0ea692d4b
Sven Lukas <sven.lukas@gmail.com>
2024-10-18 10:33:25 +0200

    updated build scripts

--- cmake/Packages.cmake
+++ cmake/Packages.cmake
@@ -1,31 +1,57 @@
 include(FetchContent)
+#include(FindPkgConfig)
 
-function(find_package_esa)
+if(ESL_DEPENDENCIES_USE_CONAN)
+    message(STATUS "Using Conan")
+    include(${CMAKE_BINARY_DIR}/conan/conan_toolchain.cmake)
+endif()
+
+if(ESL_DEPENDENCIES_USE_VCPKG)
+    message(STATUS "Using VCPKG")
+    if(WIN32)
+        set(USER_HOME_DIRECTORY $ENV{USERPROFILE})
+    else()
+        set(USER_HOME_DIRECTORY $ENV{HOME})
+    endif()
+    message(STATUS "User Home Directory: ${USER_HOME_DIRECTORY}")
+    include(${USER_HOME_DIRECTORY}/opt/vcpkg/scripts/buildsystems/vcpkg.cmake)
+endif()
+
+if(ESL_DEPENDENCIES_USE_PKGCONFIG)
+    find_package(PkgConfig QUIET)
+endif()
+
+function(find_custom_package NAME URL TAG)
     # Default, try 'find_package'. VCPKG or Conan may be used, if enabled
-    if(NOT esa_FOUND)
-        message(STATUS "Try to find esa by find_package")
-        find_package(esa QUIET)
-        if(esa_FOUND)
-            message(STATUS "esa has been found by using find_package")
+    if(NOT ${NAME}_FOUND)
+        message(STATUS "Try to find ${NAME} by find_package")
+        find_package(${NAME} QUIET)
+        if(${NAME}_FOUND)
+            message(STATUS "${NAME} has been found by using find_package")
         endif()
     endif()
 
-    if(NOT esa_FOUND)
-        message(STATUS "Try to find esa by FetchContent")
+    if(NOT ${NAME}_FOUND)
+        message(STATUS "Try to find ${NAME} by FetchContent")
+        set(COMPILE_UNITTESTS OFF)
         FetchContent_Declare(
-            esa
-            GIT_REPOSITORY https://github.com/SLukasDE/esa
-            #GIT_TAG master
+            ${NAME}
+            GIT_REPOSITORY ${URL}
+            GIT_TAG ${TAG}
             GIT_SHALLOW TRUE
             OVERRIDE_FIND_PACKAGE # 'find_package(...)' will call 'FetchContent_MakeAvailable(...)'
         )
-        find_package(esa QUIET)
-        if(esa_FOUND)
-            message(STATUS "esa has been found by using FetchContent")
+        find_package(${NAME} QUIET)
+        if(${NAME}_FOUND)
+            message(STATUS "${NAME} has been found by using FetchContent")
         endif()
     endif()
 
-    if(NOT esa_FOUND)
-        message(FATAL_ERROR "esa NOT found")
+    if(NOT ${NAME}_FOUND)
+        message(FATAL_ERROR "${NAME} NOT found")
     endif()
+endfunction()
+
+function(find_package_esa)
+    find_custom_package(esa https://github.com/SLukasDE/esa master)
 endfunction()
