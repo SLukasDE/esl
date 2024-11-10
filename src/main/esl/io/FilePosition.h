bab7a579a57cdd4ffe94c528dfec0570e4f4de71
Sven Lukas <sven.lukas@gmail.com>
2024-11-10 19:27:04 +0100

    Fixed an bug in Exception-Injector for FilePosition

--- src/main/esl/io/FilePosition.h
+++ src/main/esl/io/FilePosition.h
@@ -66,19 +66,19 @@ public:
 	static const FilePosition* get(const E& e);
 
 private:
-	std::string fileName;
-	int lineNo;
+	const std::string fileName;
+	const int lineNo;
 };
 
 template <class E>
 FilePosition::Injector<E> FilePosition::add(const std::string& fileName, int lineNo, const E& e) {
-	return FilePosition::Injector<E>(e, FilePosition(fileName, lineNo));
+	return FilePosition::Injector<E>(e, std::unique_ptr<FilePosition>(new FilePosition(fileName, lineNo)));
 }
 
 template <class E>
 const FilePosition* FilePosition::get(const E& e) {
 	const FilePosition::Injector<E>* injector = dynamic_cast<const FilePosition::Injector<E>*>(&e);
-    return injector ? &injector->filePosition : nullptr;
+	return injector ? injector->filePosition.get() : nullptr;
 }
 
 } /* namespace io */
